dfcbb239671ffdc39eb371c8fa7f66e81b7f9d07
Devrangasamy <devbhooma@gmail.com>
2023-05-07 20:52:16 +0530

    "view filght search"

--- user/src/components/Viewflights.js
+++ user/src/components/Viewflights.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Card, Button, Row, Col } from 'react-bootstrap';
+import { Card, Button, Row, Col,Form, FormGroup, FormControl } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlane } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import Adminnavbar from './Adminnavbar';
 
 const Viewflights = () => {
+  const [flightname,setflightname]=useState('');
+  const [from,setfrom]=useState('');
+  const [to,setto]=useState('');
+  const [depature,setdepature]=useState("2010-01-20T00:01");
+  const [arrival,setarrival]=useState("2025-12-07T23:59");
+  const [duration,setduration]=useState(1000);
   const styles={
     transition: 'all 0.2s ease-in-out', // add transition effect
     boxShadow: '1px 3px 7px rgba(0, 0, 0, 0.9)', // add box shadow effect
@@ -14,17 +20,19 @@ const Viewflights = () => {
   const [flights, setFlights] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:8000/api/flight')
+    axios.get(`http://localhost:8000/api/flight?flightname=${flightname}&from=${from}&to=${to}&duration=${duration}
+    &depature=${new Date(depature).toISOString()}&arrival=${new Date(arrival).toISOString()}`)
       .then(res => {
         setFlights(res.data);
       })
       .catch(err => console.log(err));
-  }, []);
+  }, [flightname,from,to,duration,depature,arrival]);
 
   const handleDelete = async (bookingId) => {
     try {
       await axios.delete(`http://localhost:8000/api/flight/${bookingId}`);
-      const response = await axios.get('http://localhost:8000/api/flight/');
+      const response = await axios.get(`http://localhost:8000/api/flight?flightname=${flightname}&from=${from}&to=${to}&duration=${duration}
+      &depature=${new Date(depature).toISOString()}&arrival=${new Date(arrival).toISOString()}`);
       setFlights(response.data);
     } catch (error) {
       console.log(error);
@@ -35,6 +43,61 @@ const Viewflights = () => {
       <Adminnavbar></Adminnavbar>
       <h2 className="text-center mb-4">Flight Details</h2>
       <br />
+      <Row className="justify-content-center mt-4">
+      <Col md={3} >
+      <Form >
+      <FormGroup md={2}>
+        <Col sm={5} >
+          Flight Name
+        </Col>
+        <Col sm={10}>
+          <FormControl type="text" placeholder="Enter flight name" onChange={(e)=>setflightname(e.target.value)} />
+        </Col>
+      </FormGroup>
+      <FormGroup>
+        <Col sm={2}>
+          From
+        </Col>
+        <Col sm={10}>
+          <FormControl type="text" placeholder="Enter departure location" onChange={(e)=>setfrom(e.target.value)}/>
+        </Col>
+      </FormGroup>
+      <FormGroup>
+        <Col sm={2}>
+          To
+        </Col>
+        <Col sm={10}>
+          <FormControl type="text" placeholder="Enter arrival location" onChange={(e)=>setto(e.target.value)}/>
+        </Col>
+      </FormGroup>
+      <FormGroup>
+        <Col sm={5}>
+          Departure Time
+        </Col>
+        <Col sm={10}>
+          <FormControl type="datetime-local" onChange={(e)=>setdepature(e.target.value)}/>
+        </Col>
+      </FormGroup>
+      <FormGroup>
+        <Col sm={6}>
+          Arrival Time
+        </Col>
+        <Col sm={10}>
+          <FormControl type="datetime-local" onChange={(e)=>setarrival(e.target.value)} />
+        </Col>
+      </FormGroup>
+      <FormGroup>
+        <Col sm={2}>
+          Duration
+        </Col>
+        <Col sm={10}>
+          <FormControl type="text" placeholder="Enter flight duration" onChange={(e)=>setduration(e.target.value)} />
+        </Col>
+      </FormGroup>
+    </Form>
+    </Col >
+    </Row>
+    <br></br>
       <Row>
         {flights.map(flight => (
           <Col md={4} key={flight._id}>
